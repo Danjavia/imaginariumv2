8f08d4537ffad76775f2870891e9c34005000f5b
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2015-12-15 21:44:15 -0500

    trying to test errors with users

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -48,10 +48,18 @@ var LoginForm = React.createClass({
                     company: authData.password.company
                 });
 
-                trackJs.configure({
-                    userId: authData.password.email, 
-                    sessionId: authData.uid
-                });
+                trackJs.configure({ userId: authData.password.email });
+
+                try {
+                    a;
+                } catch ( e ) {
+
+                    trackJs.track( e );
+
+                    trackJs.track( 'User in session error' ,{
+                        userId: authData.password.email
+                    });
+                }
 
                 // The identify code should be added before the "track()" function
                 woopra.track( "Access to app", {
